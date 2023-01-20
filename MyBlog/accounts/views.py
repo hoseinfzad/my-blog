60521403ab426d19652f3b84529f5e460d0ee7c4
hoseinfzad <hosein.fereydonizad@gmail.com>
2023-01-20 19:24:27 +0330

    implementing authentitation
    create accounts app and implement very simple authenticate system in our blog

--- MyBlog/accounts/views.py
+++ MyBlog/accounts/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 
 
 def login_view(request):
-    return render('login.html',)
-
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form':form})
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            
+        else:
+            return render(request, 'accounts/login.html', {'form':form})
+            
+   
+@login_required
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect('/')
+        
 
 def signup_view(request):
-    return render('signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+            
+        
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
